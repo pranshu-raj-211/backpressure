af3540c5af48809a43f99791e7ed20bc2ad6d774
pranshu-raj-211 <pranshuraj65536@gmail.com>
2025-09-16 13:07:37 +0530

    add proper scaling, fix send on close panic
    Signed-off-by: pranshu-raj-211 <pranshuraj65536@gmail.com>

--- examples/worker_pool/main.go
+++ examples/worker_pool/main.go
@@ -9,47 +9,103 @@ import (
 
 var wg sync.WaitGroup
 
-func worker(id int, tasks <-chan int) {
+func worker(id int, tasks <-chan int, quit <-chan struct{}) {
 	defer wg.Done()
-	for t := range tasks {
-		fmt.Printf("worker %d: processing %d\n", id, t)
-		time.Sleep(400 * time.Millisecond)
+	for {
+		select {
+		case t, ok := <-tasks:
+			if !ok {
+				fmt.Printf("worker %d: exiting (channel closed)\n", id)
+				return
+			}
+			fmt.Printf("worker %d: processing %d\n", id, t)
+			time.Sleep(400 * time.Millisecond)
+		case <-quit:
+			// consumer sends a quit signal (analogy - master process of Nginx)
+			fmt.Printf("worker %d: exiting (quit signal)\n", id)
+			return
+		}
 	}
-	fmt.Printf("worker %d: exiting\n", id)
 }
 
 func main() {
-	tasks := make(chan int, 5)
-	wg.Add(1)
-	go worker(1, tasks)
+	tasks := make(chan int, 10)
+	stopProducer := make(chan struct{})
 
-	workerCount := 1
-	taskID := 0
+	workerQuitChans := make(map[int]chan struct{})
+	workerCount := 0
+	var mu sync.Mutex
+
+	startWorker := func() {
+		mu.Lock()
+		defer mu.Unlock()
+		workerCount++
+		id := workerCount
+		quit := make(chan struct{})
+		workerQuitChans[id] = quit
+		wg.Add(1)
+		go worker(id, tasks, quit)
+		fmt.Printf("controller: started worker %d (total=%d)\n", id, workerCount)
+	}
+
+	stopWorker := func() {
+		mu.Lock()
+		defer mu.Unlock()
+		if workerCount == 0 {
+			return
+		}
+		// stop a worker - scale down
+		quit := workerQuitChans[workerCount]
+		close(quit)
+		delete(workerQuitChans, workerCount)
+		workerCount--
+		fmt.Printf("controller: stopped a worker (total=%d)\n", workerCount)
+	}
+
+	// init onw worker
+	startWorker()
 
 	// producer
 	go func() {
+		taskID := 0
 		for {
-			taskID++
 			select {
-			case tasks <- taskID:
-				// normal case, channel not at capacity
-				fmt.Printf("producer: queued %d\n", taskID)
+			case <-stopProducer:
+				close(tasks)
+				return
 			default:
-				// channel full, add another worker
-				workerCount++
-				fmt.Printf("producer: channel full, adding worker %d\n", workerCount)
-				wg.Add(1)
-				go worker(workerCount, tasks)
+				taskID++
 				tasks <- taskID
+				fmt.Printf("producer: queued %d\n", taskID)
+				time.Sleep(time.Duration(rand.Intn(200)+50) * time.Millisecond)
 			}
-
-			// vary production rate
-			time.Sleep(time.Duration(rand.Intn(200)+50) * time.Millisecond)
 		}
 	}()
 
-	time.Sleep(5 * time.Second)
-	close(tasks)
+	// consumer (controls number of workers according to load)
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+			queueLen := len(tasks)
+			queueCap := cap(tasks)
+
+			if queueLen > queueCap/2 {
+				startWorker() // scale up
+			} else if queueLen == 0 && workerCount > 1 {
+				stopWorker() // scale down
+			}
+		}
+	}()
+
+	time.Sleep(10 * time.Second)
+	close(stopProducer)
+
+	mu.Lock()
+	for _, quit := range workerQuitChans {
+		close(quit)
+	}
+	mu.Unlock()
+
 	wg.Wait()
 	fmt.Println("done")
 }
